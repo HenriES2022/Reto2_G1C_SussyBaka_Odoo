7a1eeda2f27abf18731fabe3adbb279fd16eb8a6
ioritz <ioritzurkiri2002@gmail.com>
2023-01-26 10:24:48 +0100

    view modified and some validations added in model

--- models/subject.py
+++ models/subject.py
@@ -1,4 +1,7 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+from datetime import datetime
+import re
 
 #The subject class for the 
 class Subject(models.Model):
@@ -20,3 +23,25 @@ class Subject(models.Model):
     #The teachers that are specialized in this subject
     teachers_ids = fields.Many2many('res.users', string="teachers")
     
+    #Validates that century field can only contains roman characters
+    @api.constrains('century')
+    def _check_century_only_roman_characters(self):
+        if not re.match("^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$", self.century):
+            raise ValidationError("Century field only can have roman characters and yo have %s: " % self.century)
+
+    #todo validates that introduced century is not bigger than the actual
+    @api.constrains('century')
+    def _check_century_is_not_bigger_than_actual(self):
+        current_year = datetime.now().year
+        current_century = None
+        
+        if current_year % 100 != 0
+            current_century = (current_year // 100) + 1
+        
+        else
+            current_century = current_year // 100
+        
+        if self.century > current_century
+            
+    
+        
