07ed5291df4f0b420e857c7018f015e44648cebd
henries2022 <henriqueyeguo.yeguo@gmail.com>
2023-01-27 12:30:35 +0100

    Check dates (post creation date older than course) and the same with comment

--- models/comment.py
+++ models/comment.py
@@ -13,3 +13,14 @@ class Comment(models.Model):
 
     # Fields
     comment_text = fields.Text('Comment')
+
+    @api.onchange('create_date')
+    def _check_comment_date_after_post(self):
+        comment_date = fields.Datetime.from_string(self.create_date)
+        post_date = fields.Datetime.from_String(self.post_id.create_date)
+        if comment_date < post_date:
+            return {
+                'warning' : {
+                    'title': 'Warning', 
+                    'message': 'Comment creation date can\'t be older than the cration of the post'}
+            }    
